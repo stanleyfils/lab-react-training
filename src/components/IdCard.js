8b12d77f44c04e703001de528c1d2e652bc30ad1
Stanley Fils <stanleyfils@gmail.com>
2020-05-19 20:29:59 -0400

    Add Id card component

--- src/components/IdCard.js
+++ src/components/IdCard.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 
+// Iteration 1: Component: ID Card
+// Create a IdCard component with 6 props:
+
+// lastName: A string
+// firstName: A string
+// gender: A string, 'male' or 'female'
+// height: A number
+// birth: A date
+// picture: A string
+
 class IdCard extends Component {
 	render() {
 		const { firstName, lastName, gender, height, birth, picture } = this.props;
